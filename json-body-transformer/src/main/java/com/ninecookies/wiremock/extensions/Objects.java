877b67e60d2153fac2ca398629c63fa8db9adcc1
Michael Scheepers <michael.scheepers@deliveryhero.com>
2019-03-29 13:18:25 +0100

    refactored json body transformer and extracted placeholder handling

---
+++ json-body-transformer/src/main/java/com/ninecookies/wiremock/extensions/Objects.java
@@ -0,0 +1,41 @@
+package com.ninecookies.wiremock.extensions;
+
+import java.util.regex.Matcher;
+
+import com.jayway.jsonpath.DocumentContext;
+
+public class Objects {
+
+    public static String describe(Object object) {
+        if (object == null) {
+            return null;
+        }
+        return "[" + object.getClass().getSimpleName() + "]" + object;
+    }
+
+    public static String describe(DocumentContext documentContext) {
+        if (documentContext == null) {
+            return null;
+        }
+        return "[" + documentContext.getClass().getSimpleName() + "]" + documentContext.jsonString();
+    }
+
+    public static String describe(Matcher matcher) {
+        if (matcher == null) {
+            return null;
+        }
+        String matcherString = matcher.toString();
+        StringBuilder result = new StringBuilder(matcherString.substring(0, matcherString.length() - 1));
+        result.append(" groups(").append(matcher.groupCount()).append(")=");
+        for (int i = 1; i < matcher.groupCount() + 1; i++) {
+            if (i > 1) {
+                result.append(",");
+            }
+            result.append("[").append(i).append("]").append(matcher.group(i));
+        }
+        return result.toString();
+    }
+
+    protected Objects() {
+    }
+}
