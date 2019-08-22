9414135aa7cb24cb7a4c62149e3e293a0c7f33dc
Tarso Bessa <tarso.bessa@gmail.com>
2019-08-22 15:55:54 +0200

    [RCS - 398] - Limiting Wiremock max memory

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -13,7 +13,7 @@ fi
 
 # Add `java -jar /wiremock-standalone.jar` as command if needed
 if [ "${1:0:1}" = "-" ]; then
-	set -- java -Xms2048m -Xmx4096m -Dlog4j.configurationFile="/var/wiremock/lib/log4j2.xml" \
+	set -- java -Xms2048m -Xmx3072m -Dlog4j.configurationFile="/var/wiremock/lib/log4j2.xml" \
 	-cp /var/wiremock/lib/*:/var/wiremock/extensions/* \
 	com.github.tomakehurst.wiremock.standalone.WireMockServerRunner \
 	--extensions com.ninecookies.wiremock.extensions.JsonBodyTransformer,com.ninecookies.wiremock.extensions.CallbackSimulator \
