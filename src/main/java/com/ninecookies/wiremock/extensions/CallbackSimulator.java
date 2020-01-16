9c3beef6e3e5121eb3d1433c65163efc151fa586
Michael Scheepers <michael.scheepers@deliveryhero.com>
2020-01-16 10:01:39 +0100

    persist callback definition in file system

--- src/main/java/com/ninecookies/wiremock/extensions/CallbackSimulator.java
+++ src/main/java/com/ninecookies/wiremock/extensions/CallbackSimulator.java
@@ -1,7 +1,11 @@
 package com.ninecookies.wiremock.extensions;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.Executors;
@@ -87,18 +91,55 @@ public class CallbackSimulator extends PostServeAction {
         Callbacks callbacks = parameters.as(Callbacks.class);
 
         for (Callback callback : callbacks.callbacks) {
-            LOG.debug("callback.data: {}", Objects.describe(callback.data));
-            String dataJson = Placeholders.transformJson(servedJson, Json.write(callback.data));
-            LOG.debug("final data: {}", dataJson);
-            Object url = Placeholder.containsPattern(callback.url)
-                    ? Placeholder.of(callback.url).getSubstitute(servedJson)
-                    : callback.url;
-            LOG.info("scheduling callback task to: '{}' with delay '{}' and data '{}'", url, callback.delay, dataJson);
+            Callback normalizedCallback = normalizeCallback(servedJson, callback);
+            File callbackDefinition = persistCallback(normalizedCallback);
+            LOG.info("scheduling callback task to: '{}' with delay '{}' and data '{}'",
+                    callback.url, callback.delay, callback.data);
+            executor.schedule(CallbackHandler.of(callbackDefinition), callback.delay, TimeUnit.MILLISECONDS);
+        }
+    }
 
-            executor.schedule(
-                    PostTask.of(url.toString(), callback.authentication, callback.traceId, dataJson),
-                    callback.delay,
-                    TimeUnit.MILLISECONDS);
+    /**
+     * Normalizes the specified {@code callback} according to the specified {@code servedJson} and replaces placeholder
+     * patterns in {@link Callback#data} as well as in {@link Callback#url}.<br>
+     * In addition it ensures that the {@link Callback#traceId} is present.
+     *
+     * @param servedJson a {@link DocumentContext} representing the request and response bodies as well as the request
+     *            path.
+     * @param callback the {@link Callback} to normalize.
+     * @return the normalized {@link Callback} with replaced patterns and keywords according to the specified
+     *         {@code servedJson}.
+     */
+    private Callback normalizeCallback(DocumentContext servedJson, Callback callback) {
+        LOG.debug("url: {} data: {}", callback.url, Objects.describe(callback.data));
+        callback.data = Placeholders.transformJson(servedJson, Json.write(callback.data));
+        if (Placeholder.containsPattern(callback.url)) {
+            callback.url = Placeholder.of(callback.url).getSubstitute(servedJson);
+        }
+        if (callback.traceId == null) {
+            callback.traceId = UUID.randomUUID().toString().replace("-", "");
+        }
+        LOG.debug("final url: {} data: {}", callback.url, callback.data);
+        return callback;
+    }
+
+    /**
+     * Persists the specified {@code callback} as temporary file in the file system to be picked up by the
+     * scheduled {@link CallbackHandler} when due to reduce the memory footprint during callback handling.
+     *
+     * @param callback the {@link Callback} to persist.
+     * @return the temporary {@link File} containing the normalized callback definition.
+     */
+    private File persistCallback(Callback callback) {
+        try {
+            File result = File.createTempFile("callback-json-", ".tmp");
+            LOG.debug("callback-json file: {}", result);
+            String jsonContent = Json.write(callback);
+            LOG.debug("callback-json content: {}", jsonContent);
+            Files.write(result.toPath(), jsonContent.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
+            return result;
+        } catch (IOException e) {
+            throw new IllegalStateException("unable to persist callback data", e);
         }
     }
 
@@ -106,12 +147,13 @@ public class CallbackSimulator extends PostServeAction {
      * Implements {@link ThreadFactory} producing daemon threads ({@link Thread#isDaemon()} is {@code true}) to use
      * with {@link ScheduledExecutorService} to avoid that {@link CallbackSimulator} blocks WireMock shutdown.
      */
-    private static final class DaemonThreadFactory implements ThreadFactory {
+    // VisibleForTesting
+    static final class DaemonThreadFactory implements ThreadFactory {
         private static final AtomicInteger POOL_NUMBER = new AtomicInteger(1);
         private final AtomicInteger threadNumber = new AtomicInteger(1);
         private final String name;
 
-        private DaemonThreadFactory() {
+        DaemonThreadFactory() {
             name = "callback-timer-" + POOL_NUMBER.getAndIncrement() + "-thread-";
         }
 
@@ -130,27 +172,30 @@ public class CallbackSimulator extends PostServeAction {
 
     /**
      * Implements {@link Runnable} and uses {@link HttpPost} in combination with {@link HttpEntity} and
-     * {@link HttpContext} to POST the specified callback JSON payload to the specified callback URL.
+     * {@link HttpContext} to emit a POST request according to the referenced callback definition.
      */
-    private static final class PostTask implements Runnable {
+    private static final class CallbackHandler implements Runnable {
 
+        private static final String RPS_TRACEID_HEADER = "X-Rps-TraceId";
         private static final RequestConfig REQUEST_CONFIG = RequestConfig.custom()
                 .setSocketTimeout(2_000)
                 .setConnectTimeout(3_000)
                 .setConnectionRequestTimeout(5_000)
                 .build();
 
-        private URI uri;
-        private HttpEntity content;
-        private HttpContext context;
-        private String traceId;
+        private File callbackFile;
 
         @Override
         public void run() {
+            Callback callback = readCallback();
+            URI uri = URI.create(callback.url);
+
+            HttpContext context = createHttpContext(uri, callback.authentication);
+            HttpEntity content = new StringEntity((String) callback.data, ContentType.APPLICATION_JSON);
             HttpPost post = new HttpPost(uri);
             post.setConfig(REQUEST_CONFIG);
+            post.addHeader(RPS_TRACEID_HEADER, callback.traceId);
             post.setEntity(content);
-            post.addHeader("X-Rps-TraceId", traceId);
 
             try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
                 HttpResponse response = client.execute(post, context);
@@ -165,8 +210,23 @@ public class CallbackSimulator extends PostServeAction {
                 }
             } catch (Exception e) {
                 // in failure case print request body and exception
-                LOG.error("post to '{}' errored\ncontent\n{}\n{}",
-                        uri, content, readEntity(content), e);
+                LOG.error("post to '{}' errored\ncontent {}\n{}", uri, content, readEntity(content), e);
+            } finally {
+                try {
+                    Files.deleteIfExists(callbackFile.toPath());
+                } catch (IOException e) {
+                    LOG.error("unable to delete callback definition file", e);
+                }
+            }
+        }
+
+        private Callback readCallback() {
+            try {
+                String jsonContent = new String(Files.readAllBytes(callbackFile.toPath()), StandardCharsets.UTF_8);
+                return Json.read(jsonContent, Callback.class);
+                // return new StringEntity(jsonContent, ContentType.APPLICATION_JSON);
+            } catch (IOException e) {
+                throw new IllegalStateException("Unable to read callback content from file system", e);
             }
         }
 
@@ -182,16 +242,7 @@ public class CallbackSimulator extends PostServeAction {
             return result;
         }
 
-        private static Runnable of(String uri, Authentication authentication, String traceId, String jsonContent) {
-            PostTask result = new PostTask();
-            result.uri = URI.create(uri);
-            result.content = new StringEntity(jsonContent, ContentType.APPLICATION_JSON);
-            result.context = createHttpContext(result.uri, authentication);
-            result.traceId = traceId != null ? traceId : UUID.randomUUID().toString().replace("-", "");
-            return result;
-        }
-
-        private static HttpContext createHttpContext(URI uri, Authentication authentication) {
+        private HttpContext createHttpContext(URI uri, Authentication authentication) {
             if (authentication == null) {
                 return null;
             }
@@ -213,6 +264,12 @@ public class CallbackSimulator extends PostServeAction {
             context.setCredentialsProvider(credentialsProvider);
             context.setAuthCache(authCache);
             return context;
+        }
+
+        private static Runnable of(File callbackFile) {
+            CallbackHandler result = new CallbackHandler();
+            result.callbackFile = callbackFile;
+            return result;
         }
     }
 }
