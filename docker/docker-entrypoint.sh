59aff7b79ea91105ae6100a25dbb045bf72021ce
Michael Scheepers <michael.scheepers@deliveryhero.com>
2020-05-19 14:18:48 +0200

    adapt docker image to respect java opts and expose jmx port

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -13,7 +13,7 @@ fi
 
 # Add `java -jar /wiremock-standalone.jar` as command if needed
 if [ "${1:0:1}" = "-" ]; then
-	set -- java -Dlog4j.configurationFile="/var/wiremock/lib/log4j2.xml" \
+	set -- java $JAVA_OPTS -Dlog4j.configurationFile="/var/wiremock/lib/log4j2.xml" \
 	-cp /var/wiremock/lib/*:/var/wiremock/extensions/* \
 	com.github.tomakehurst.wiremock.standalone.WireMockServerRunner \
 	--extensions com.ninecookies.wiremock.extensions.JsonBodyTransformer,com.ninecookies.wiremock.extensions.CallbackSimulator,com.ninecookies.wiremock.extensions.RequestTimeMatcher \
